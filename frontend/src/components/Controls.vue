5396da32e4ca2e9b778c799a36a3aa0eed1a8368
Yajy Goswami <goswamiyajy@gmail.com>
2024-12-31 04:00:27 +0530

    Implemented Keyboard Movement - using Event Listener

--- frontend/src/components/Controls.vue
+++ frontend/src/components/Controls.vue
@@ -6,19 +6,65 @@
         <ion-button @mousedown="down" @mouseup="stopMoving" @touchstart="down" @touchend="stopMoving">Down</ion-button>
     </div>
 </template>
-
-<script setup>
-
-import { IonButton } from '@ionic/vue';
-import { useMovement } from '@/composables/useMovement';
-
-const { left, right, up, down, stopMoving, movement } = useMovement();
-
-</script>
-
-<style scoped>
-#divControls {
+  
+  <script setup>
+  import { IonButton } from '@ionic/vue';
+  import { useMovement } from '@/composables/useMovement';
+  import { onMounted, onUnmounted } from 'vue';
+  
+  const { left, right, up, down, stopMoving } = useMovement();
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+    switch (event.key) {
+      case 'ArrowLeft':
+      case 'a':
+        left();
+        break;
+      case 'ArrowRight':
+      case 'd':
+        right();
+        break;
+      case 'ArrowUp':
+      case 'w':
+        up();
+        break;
+      case 'ArrowDown':
+      case 's':
+        down();
+        break;
+    }
+  };
+  
+  const handleKeyUp = (event) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+      case 'a':
+      case 'ArrowRight':
+      case 'd':
+      case 'ArrowUp':
+      case 'w':
+      case 'ArrowDown':
+      case 's':
+        stopMoving();
+        break;
+    }
+  };
+  
+ 
+  onMounted(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+  });
+  
+  onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeyDown);
+    window.removeEventListener('keyup', handleKeyUp);
+  });
+  </script>
+  
+  <style scoped>
+ #divControls {
     display: flex;
     justify-content: space-between;
 }
-</style>
+  </style>
